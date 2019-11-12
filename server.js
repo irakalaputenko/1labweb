e069b98df93a6ec98aae4789adbce27353094fa9
irakalaputenko <48915796+irakalaputenko@users.noreply.github.com>
2019-11-12 11:44:23 +0200

    Commit1

--- server.js
+++ server.js
@@ -10,43 +10,44 @@ var app = express();
 //app.use(app.router); // модуль для простого задания обработчиков путей
 app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 app.get('/api', function (req, res) {
-res.send('API is running');
+   res.send('API is running');
 });
 app.listen(1337, function(){
-console.log('Express server listening on port 1337');
-   });
+   console.log('Express server listening on port 1337');
+});
 
 
 app.use(function(req, res, next){
-res.status(404);
-log.debug('Not found URL: %s',req.url);
-res.send({ error: 'Not found' });
-return;
+   res.status(404);
+   log.debug('Not found URL: %s',req.url);
+   res.send({ error: 'Not found' });
+   return;
 });
 
 app.use(function(err, req, res, next){
-res.status(err.status || 500);
-log.error('Internal error(%d): %s',res.statusCode,err.message); res.send({ error: err.message });
- 	return;
+   res.status(err.status || 500);
+   log.error('Internal error(%d): %s', res.statusCode,err.message); 
+   res.send({ error: err.message });
+   return;
 });
 
 app.get('/ErrorExample', function(req, res, next){
-next(new Error('Random error!'));
+   next(new Error('Random error!'));
 });
 
 
 app.get('/api/articles', function(req, res) {
-res.send('This is not implemented now');
+   res.send('This is not implemented now');
 });
 app.post('/api/articles', function(req, res) {
- 	res.send('This is not implemented now');
+   res.send('Method Post Work');
 });
 app.get('/api/articles/:id', function(req, res) {
-res.send('This is not implemented now');
+   res.send('Get method');
 });
 app.put('/api/articles/:id', function (req, res){
-res.send('This is not implemented now');
+   res.send('Method Put Work');
 });
 app.delete('/api/articles/:id', function (req, res){
-res.send('This is not implemented now');
+   res.send('Method Delete Work');
 });
